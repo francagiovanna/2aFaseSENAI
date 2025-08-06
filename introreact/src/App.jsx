8042b82c1975da22e9eba577ca6fc329c0856fd3
francagiovanna <francamariagiovanna4@gail.com>
2025-08-05 21:59:34 -0300

    novos updates de conteudo

---
+++ introreact/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Navbar />
+      <h1>Página de React JS</h1>
+      <MotivoParaUsarReact />
+    </>
+  )
+}
+
+export default App
